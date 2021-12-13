9044e1e32ff6f6b4fc0db56fb90bc0f0b3ca9267
ryandelongchamps <89552468+ryandelongchamps@users.noreply.github.com>
2021-12-13 14:11:46 -0600

    part 6

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,9 @@
+from django.http.response import Http404
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -12,13 +15,18 @@ def topics(request):
     context = {'topics':topics}
     return render(request, 'MainApp/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date added')
     context = {'topic':topic, 'entries':entries}
 
+    if topic.owner != request.user:
+        raise Http404
+
     return render(request, 'MainApp/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -33,6 +41,7 @@ def new_topic(request):
     context = {'form':form}
     return render(request, 'MainApp/new_topic.html',context)
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -48,10 +57,15 @@ def new_entry(request, topic_id):
     context = {'form': form,'topic':topic}
     return render(request, 'MainApp/new_entry.html', context)
 
+@login_required
 def edit_entry(request,entry_id):
     """edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    
+    if topic.owner != request.user:
+        raise Http404
+
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
@@ -60,6 +74,6 @@ def edit_entry(request,entry_id):
         if form.is_valid():
             form.save()
             return redirect('MainApp:topic',topic_id=topic.id)
-            
+
     context = {'entry':entry, 'topic':topic, 'form':form}
     return render(request, 'MainApp/edit_entry.html', context)      
