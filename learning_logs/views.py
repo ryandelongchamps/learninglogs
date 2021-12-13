fe9f140bfaee62f5c4e930ff469714e6d5cd41d7
ryandelongchamps <89552468+ryandelongchamps@users.noreply.github.com>
2021-12-13 12:37:13 -0600

    part 3

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Topic, Topics
 
 # Create your views here.
 
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+def topics(request):
+    topics = Topic.objects.filter(owner=request.users.order_by('date_added'))
+    context = {'topics':topics}
+    return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topics.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date added')
+    context = {'topic':topic, 'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
